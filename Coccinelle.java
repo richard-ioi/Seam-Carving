56cc46c57a5191f534defb789a0bec78fe4f51b0
Jeremy LAVEILLE <jeremy.laveille@edu.esiee.fr>
2020-05-22 18:33:07 +0200

    Fonction afficheTab

--- Coccinelle.java
+++ Coccinelle.java
@@ -1,4 +1,13 @@
-
 public class Coccinelle {
+
+
+    public void afficheTab() {
+        for(int j=this.aTailleColonne-1 ; j>=0 ; j--) {
+            for(int i=0 ; i<this.aTailleLigne ; i++) {
+                System.out.print(" "+this.aTab[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 
 }
