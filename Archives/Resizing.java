8f819fe6697334dc483a967b017920055b129357
richard-ioi <richard.fouquoire@edu.esiee.fr>
2020-05-29 16:49:42 +0200

    Modification de la structure / ajout de Resizing en mode projet

--- Archives/Resizing.java
+++ Archives/Resizing.java
@@ -2,18 +2,20 @@
 // ESIEE E2 groupe 10
 
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import java.net.URL;
+import javax.imageio.*;
 
-public class Resizing{
+public class Resizing extends Applet{
 /*
 Cet algorithme correspond à l'exercice n°2 du projet de l'unité d'algorithmique du 2e semestre.
 Il permet, 
 */
 	//###ATTRIBUTS###//
-	public BufferedImage aImage;
+	public static BufferedImage aImage = null;
 
     //###FONCTION PRINCIPALE###//
     public static void main(String[] args){
@@ -25,11 +27,13 @@ Il permet,
 	/*
 
 	*/
-    public static void chargerImage(String pFileName){
+    public void chargerImage(String pFileName){
 		try{
-			BufferedImage aImage = ImageIO.read(new File(pFileName));
+			URL url = new URL(getCodeBase(), "chaton.jpg");
+			aImage = ImageIO.read(new File(url));
+			//aImage = ImageIO.read(new File("chaton.jpg"));
 			//System.out.println(aImage);
-			System.out.println("L'image est bien chargée");
+			//System.out.println("L'image est bien chargée");
 		}
 		catch (IOException e){
 			System.out.println("L'image ne s'est pas chargée");
---
+++ Exercice2 (Redimensionnement d'images)/src/Resizing.java
@@ -0,0 +1,31 @@
+/*
+Cet algorithme correspond à l'exercice n°2 du projet de l'unité d'algorithmique du 2e semestre.
+Il permet,
+*/
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+
+public class Resizing{
+    //###ATTRIBUTS###//
+    public static BufferedImage aImage = null;
+
+    //###FONCTION PRINCIPALE###//
+    public static void main(String[] args){
+        chargerImage("images/chaton.jpg");
+    }
+
+    //###AUTRES FONCTIONS##//
+
+    public static void chargerImage(String pFileName){
+        try{
+            aImage = ImageIO.read(new File(pFileName));
+            System.out.println("L'image s'est bien chargée");
+            System.out.println("Informations de l'image:");
+            System.out.println(aImage);
+        }
+        catch (IOException e){
+            System.out.println("L'image ne s'est pas chargée");
+        }
+    }
+}
