a5b2b3510b8c1e79a6d9e1116cdbe2497416bbfa
Jeremy LAVEILLE <jeremy.laveille@edu.esiee.fr>
2020-05-29 11:50:37 +0200

    Probleme import

--- Resizing.java
+++ Resizing.java
@@ -1,6 +1,15 @@
 // Richard FOUQUOIRE et Jérémy LAVEILLE - 29/05/2020
 // ESIEE E2 groupe 10
 
+import java.awt;
+import java.ImageIO;
+import java.ImageIcon;
+import java.File;
+import java.JFrame;
+import java.JLabel;
+import java.jPanel;
+import java.Dimension;
+
 public class Resizing {
 /*
 Cet algorithme correspond à l'exercice n°2 du projet de l'unité d'algorithmique du 2e semestre.
@@ -12,8 +21,19 @@ Il permet,
     
     
     // ###FONCTION PRINCIPALE###
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        String imagePath = "Images/chaton1.png";
+        BufferedImage myPicture = ImageIO.read(new File(imagePath));
+
+        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+
+        JPanel jPanel = new JPanel();
+        jPanel.add(picLabel);
+
+        JFrame f = new JFrame();
+        f.setSize(new Dimension(myPicture.getWidth(), myPicture.getHeight()));
+        f.add(jPanel);
+        f.setVisible(true);
     }
 
     //###AUTRES FONCTIONS###
