a7521f88dd0c4742459f4ae2fd86d640acd16bac
richard-ioi <richard.fouquoire@edu.esiee.fr>
2020-05-30 11:10:16 +0200

    Ajout des données de la largeur, hauteur du tableau ainsi que de la méthode getRGBTab qui retourne un tableau avec les valeurs RGB de chaque pixel

--- Exercice2/SeamCarving.java
+++ Exercice2/SeamCarving.java
@@ -13,10 +13,13 @@ public class SeamCarving{
     public static int aPourcentageHorizontal;
     public static int aPourcentageVertical;
     public static String aNomImage;
+    public static int aLargeurImage;
+    public static int aHauteurImage;
 
     //###FONCTION PRINCIPALE###//
     public static void main(String[] args){
         Initialisation(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        System.out.println("Valeur RGB Du tableau en 0, 0:"+getRGBTab(aImage)[0][0]);
     }
 
     //###AUTRES FONCTIONS###//
@@ -39,14 +42,29 @@ public class SeamCarving{
     public static void chargerImage(String pFileName){
         try{
             aImage = ImageIO.read(new File(pFileName));
+            aLargeurImage = aImage.getWidth();
+            aHauteurImage = aImage.getHeight();
             System.out.println("L'image s'est bien chargée");
             //System.out.println("Informations de l'image:");
             //System.out.println(aImage);
             System.out.println("Le nom de l'image est : "+aNomImage);
+            System.out.println("Largeur de l'image : "+aLargeurImage+"px, Hauteur de l'image : "+aHauteurImage+"px");
             System.out.println("L'image doit être réduite de "+aPourcentageHorizontal+"% en largeur et "+aPourcentageVertical+"% en hauteur");
         }
         catch (IOException e){
             System.out.println("L'image ne s'est pas chargée, vérifiez le nom.");
         }
+    }
+
+    public static int[][] getRGBTab(BufferedImage pImage){
+        int pLargeur=pImage.getWidth();
+        int pHauteur=pImage.getHeight();
+        int[][] RGBTab = new int[pLargeur][pHauteur];
+        for (int i=0;i<pLargeur;i++){
+            for (int j=0;j<pHauteur;j++){
+                RGBTab[i][j]=pImage.getRGB(i,j);
+            }
+        }
+        return RGBTab;
     }
 }
